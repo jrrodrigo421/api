548e38df0658a21c8b23acd5db75f4666ec9a9fb
jrrodrigo42 <rjr89000@gmail.com>
2024-05-04 18:08:27 -0300

    feat: adjusteds entitys

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,20 +1,3 @@
-// import { Request, Response } from 'express';
-// import UserModel from '../models/user';
-
-// export const loginAuth = async (req: Request, res: Response) => {
-  
-//   try {
-//     // const users = await UserModel.find();
-//     // res.json(users);
-//     console.log('logoou');
-//     res.json('LOGADO!')
-    
-//   } catch (error: any) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,32 +1,24 @@
-// import mongoose, { Schema, Document } from 'mongoose';
-
-// export interface User extends Document {
-// name: string;
-// email: string;
-// password: string;
-
-// }
-
-// const UserSchema: Schema = new Schema({
-// name: { type: String, required: true },
-// email: { type: String, required: true, unique: true },
-// password: { type: String, required: true}
-// });
-
-// export default mongoose.model('User', UserSchema);
-
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface User extends Document {
   name: string;
   email: string;
   password: string;
+  address: string;
+  city: string,
+  cpf: number;
+  phone: number,
 }
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  address: {type: String, required: true},
+  city: {type: String, required: true},
+  cpf: {type: Number, required: true},
+  phone: {type: Number, required: true},
+  
 });
 
 export default mongoose.model<User>('User', UserSchema);
